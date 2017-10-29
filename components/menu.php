42ffd1f2d93e8034ae94bb3b15c30636e79d0bf9
sovietspy2 <sovietspy2@yandex.com>
2017-10-29 19:35:16 +0100

    2

---
+++ components/menu.php
@@ -0,0 +1,7 @@
+<?php
+    print '<ul>';
+    print '<li><a href="index.php">Home</a></li>';
+    print '<li><a href="index.php?page=info">INFO</a></li>';
+    print '<li><a href="index.php?page=news">NEWS</a></li>';
+    print '</ul>';
+?>
--- index.php
+++ index.php
@@ -3,6 +3,12 @@
         <h1> Welcome </h1>
     </head>
     <?php
-        print 'user';
+       include "components/menu.php";
+
+    if( isset( $_GET['page'] ) )
+    {
+        $page=$_GET['page'];
+        include "components/".$page.".php";
+    }
     ?>
 </html>
